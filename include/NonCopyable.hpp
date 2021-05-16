011a7f4beb4c997886553a1ad02f76a1b03f5ab4
cava <francescocava95@gmail.com>
2021-05-16 17:30:44 +0200

    First: initial stuff

---
+++ include/NonCopyable.hpp
@@ -0,0 +1,24 @@
+#ifndef UTILS_INCLUDE_NONCOPYABLE_HPP
+#define UTILS_INCLUDE_NONCOPYABLE_HPP
+
+/**
+ * @brief Non-copyable Mixin.
+ * Modified from: https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Non-copyable_Mixin
+ * @tparam T
+ */
+template <class T>
+class NonCopyable {
+public:
+    NonCopyable(const NonCopyable &) = delete;
+    NonCopyable(NonCopyable &&) noexcept = default;  // only movable objects
+    NonCopyable &operator=(const NonCopyable &) = delete;
+    NonCopyable &operator=(NonCopyable &&) noexcept = delete;
+    T &operator=(const T &) = delete;
+    T &operator=(T &&) noexcept = delete;
+
+protected:
+    NonCopyable() = default;
+    ~NonCopyable() = default;  /// Protected non-virtual destructor
+};
+
+#endif
